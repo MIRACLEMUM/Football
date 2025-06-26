bf597dde867a6bcdc55e52e07ef9c63789a6f001
Miracle sunady <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2025-06-26 19:38:51 +0100

    jjj

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Tailwind + React is Working! 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
